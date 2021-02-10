4956a4312c0e7b7378f858f6f26bca4602af242f
AdamELO <59090327+AdamELO@users.noreply.github.com>
2021-02-10 18:46:12 +0100

    ui design chat + menu + list pt1

--- resources/views/livewire/menu.blade.php
+++ resources/views/livewire/menu.blade.php
@@ -1,10 +1,10 @@
-    <div class="w-2/5 p-5 mx-auto text-center">
+    <div class="p-5 mx-auto text-center">
         <ul class="list-none ulChat text-orange-300">
-            <div class="bg-orange-300 p-3 ulmenu text-black rounded-lg cursor-pointer">Menu <i class="float-right fas fa-caret-down icone"></i></div>
-            <li class="p-3 w-3/4 menuli mx-auto liChat rounded-lg bg-white"><a href="/editprofile">Edit Profile</a></li>
-            <li class="p-3 w-3/4 menuli mx-auto liChat rounded-lg bg-white"><a href="/friendlist">My Friends</a></li>
-            <li class="p-3 w-3/4 menuli mx-auto liChat rounded-lg bg-white"><a href="/addfriend">Add friends</a></li>
-            <li class="p-3 w-3/4 menuli mx-auto liChat rounded-lg bg-white"><a href="/friendrequest">Pending friend request</a></li>
+            {{-- <div class="bg-orange-300 p-3 ulmenu text-black rounded-lg cursor-pointer">Menu <i class="float-right fas fa-caret-down icone"></i></div> --}}
+            {{-- <li class="p-3 menuli mx-auto liChat rounded-lg bg-white"><a href="/editprofile">Edit Profile</a></li> --}}
+            {{-- <li class="p-3 menuli mx-auto liChat rounded-lg bg-white"><a href="/friendlist">My Friends</a></li> --}}
+            <li class="p-3 menuli mx-auto liChat rounded-lg bg-white"><a href="/addfriend"><i class="fas fa-user-plus color-orange-300"></i> &nbsp;Add friends</a></li>
+            <li class="p-3 menuli mx-auto liChat rounded-lg bg-white"><a href="/friendrequest"><i class="fas fa-id-badge color-orange-300"></i>&nbsp;Pending friend request</a></li>
         </ul>
     </div>
 
--- resources/views/livewire/uichat.blade.php
+++ resources/views/livewire/uichat.blade.php
@@ -1,31 +1,52 @@
 <div>
     <div class="grid grid-rows-3 grid-flow-col mt-2">
         <div class="row-span-3 rounded-lg bg-blue-100 mx-2" style="height: 80vh">
-            <div class="ml-2">
-                <i class="fas fa-bars"></i>&nbsp; Side Menu
-            </div>
             <div class="rounded-lg bg-gray-100 mx-2">
-                <div class="my-3 flex justify-center">
-                    <h1>My Friends &nbsp;</h1>
-                    <span class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs text-black bg-gray-300 rounded-full">
-                        {{count($friends)}}
+                <div class="my-3 flex justify-center mt-5">
+                    <h1 class="mt-1">My Friends &nbsp;</h1>
+                    <span
+                        class="inline-flex items-center justify-center px-2 py-1 mr-2 mt-1 text-xs text-black bg-gray-300 rounded-full">
+                        {{ count($friends) }}
                     </span>
                 </div>
                 <div class="mx-2">
                     @foreach ($friends as $friend)
-                    <div class="flex my-3">
-                        <img width="50px" height="50px" class="rounded-full" src="./img/{{ $friend->avatar }}"
-                            alt="avatar friend">
-                        <h1 class="ml-2 inline-flex items-center justify-center text-gray-600">{{ $friend->name }}</h1>
-                    </div>
+                        <div class="flex my-3 flex justify-between items-center">
+                            <img width="50px" height="50px" class="rounded-full" src="./img/{{ $friend->avatar }}"
+                                alt="avatar friend">
+                            <h1 class="ml-3 inline-flex items-center justify-center text-gray-600">
+                                {{ $friend->name }}
+                            </h1>
+                            <span class="bg-green-500 rounded-full px-1 py-1 inline-flex items-center justify-center"></span>
+                        </div>
                     @endforeach
                 </div>
-                {{$friends->links()}}
+                {{ $friends->links() }}
             </div>
         </div>
         <div class="col-span-3" style="height: 80vh">
             @livewire('chat')
         </div>
-        <div class="row-span-3 border border-black text-center" style="height: 80vh">3</div>
+        <div class="row-span-3 rounded-lg bg-blue-100 mx-2" style="height: 80vh">
+            <div class="bg-orange-300 shadow-lg w-3/4 mx-auto my-2 flex justify-center items-center flex-col rounded-lg">
+                @if (Storage::disk('public')->has(Auth::user()->avatar))
+                    <img class="rounded-full w-12 h-12 mt-2" src="{{ asset('storage/' . Auth::user()->avatar) }}" alt="">
+                @else
+                    <img class="rounded-full w-12 h-12 mt-2" src='./img/{{ Auth::user()->avatar }}' alt=""/>  
+                @endif
+                <div class="text-black">{{ Auth::user()->name }}</div>
+                <div class="text-xs text-gray-500">
+                    <div class="rounded-l-2xl rounded-r-sm mb-1 p-1 px-4 cursor-pointer bg-gray-700 text-white">
+                        <a href="/editprofile">
+                            <i class="fas fa-cog"></i>
+                            &nbsp;Edit Profile
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="rounded-lg ">
+                @livewire('menu')
+            </div>
+        </div>
     </div>
 </div>
