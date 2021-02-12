6938d9b1275887fa53d1eeea0988a54f816ec276
AdamELO <59090327+AdamELO@users.noreply.github.com>
2021-02-12 19:01:25 +0100

    addfriend finished ui

--- resources/views/livewire/addfriend.blade.php
+++ resources/views/livewire/addfriend.blade.php
@@ -3,20 +3,24 @@
             <h1 class="my-2 text-center">Find your friends now !!!</h1>
             <input type="search" wire:model='searchPeople'
                 class=" mb-8 w-4/5 flex mx-auto border-2 border-orange-300 p-2" placeholder="Search by name...">
-                <div class="rounded-lg w-2/5 bg-white">
-                    @foreach ($users as $user)
-                        <div class="flex my-3 flex">
-                            <img width="50px" height="50px" class="rounded-full" src="./img/{{ $user->avatar }}"
-                                alt="avatar friend">
-                            <h1 class="inline-flex items-center justify-center text-gray-600">
-                                {{ $user->name }}
-                            </h1>
-                            <button wire:click='addFriend({{ $user->id }})'
-                                class="px-2 py-1 rounded-xl bg-green-300 text-green-600 max-w-max shadow-sm hover:shadow-lg">Add</button>
-                        </div>
-                        <hr>
-                    @endforeach
-                </div>
+            <div class="rounded-lg w-2/3 flex flex-col mx-auto">
+                @foreach ($users as $user)
+                    <ul class="flex flex-col m-3">
+                        <li class="flex flex-row">
+                            <div
+                                class="flex flex-1 items-center transition duration-500 ease-in-out justify-around transform hover:-translate-y-2 rounded-2xl border-2 p-3 hover:shadow-2xl border-orange-300">
+                                <img width="50px" height="50px" class="rounded-full" src="./img/{{ $user->avatar }}"
+                                    alt="avatar friend">
+                                <h1 class="text-gray-600">
+                                    {{ $user->name }}
+                                </h1>
+                                <button wire:click='addFriend({{ $user->id }})'
+                                    class="rounded-lg p-1 bg-green-300 text-green-600">Add <i class="fas fa-user-plus"></i></button>
+                            </div>
+                        </li>
+                    </ul>
+                @endforeach
+            </div>
         </div>
         {{ $users->links() }}
     </div>
