21e0a3e87f1aed81d56efc7771129d8b3c8fe3f0
AdamELO <59090327+AdamELO@users.noreply.github.com>
2021-02-16 19:22:07 +0100

    ui col ok

--- resources/views/livewire/dashhome.blade.php
+++ resources/views/livewire/dashhome.blade.php
@@ -1,7 +1,21 @@
-<div class="w-3/5 p-5 bg-white mx-auto mt-5 rounded-lg">
-    <h1 class="text-orange-300">{{Auth::user()->name}}</h1>
-    <hr class="w-4/5 mb-10" style="border: solid 2px #fdba8c">
-    <p class="my-5 text-bold">Friends: {{$nbFriends}}</p>
-    <p class="my-5 text-bold">Pending friend request: {{$nbRequests}} </p>
-    <p class="my-5 text-bold">Messages waiting: coming soon</p>
+<div class="w-4/5 p-5 bg-white mx-auto mt-5 rounded-lg">
+    <h1 class="text-center text-orange-300 underline mb-4">Infos</h1>
+    <div class="grid grid-cols-3 gap-8">
+        <div class="border border-orange-300 rounded-lg p-2">
+            <img src="./img/addfriend.png" class="" alt="add_friend">
+            <p class="my-5 text-bold text-center">Number of friends: <span class="px-2 py-1 mr-2 mt-1 text-xs text-black bg-gray-300 rounded-full">{{$nbFriends}}</span></p>
+        </div>
+        <div class="border border-orange-300 rounded-lg p-2">
+            <img src="./img/addfriend.png" class="" alt="add_friend">
+            <p class="my-5 text-bold text-center">Pending requests: <span class="px-2 py-1 mr-2 mt-1 text-xs text-black bg-gray-300 rounded-full">{{$nbRequests}}</span> </p>
+        </div>
+        <div class="border border-orange-300 rounded-lg p-2">
+            <img src="./img/addfriend.png" class="" alt="add_friend">
+            <p class="my-5 text-bold text-center">Status: 
+                <span class="p-2 rounded-full {{Auth::user()->status == 1 ? 'bg-green-200 text-green-500' : 'bg-red-200 text-red-500'}}">
+                    {{Auth::user()->status == 1 ? 'Connected' : 'Absent'}}
+                </span>
+            </p>
+        </div>
+      </div>
 </div>
